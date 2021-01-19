8435c216d1e1f4352686bb9df8b53a66a4dec4fe
lyricidal <lyricidal@gmail.com>
2021-01-19 11:05:40 -0500

    Add sync fix for block 803585
    CheckPoAContainRecentHash checks previous PoA block, so we use 719456 here

--- src/poa.cpp
+++ src/poa.cpp
@@ -216,7 +216,7 @@ bool CheckPoAContainRecentHash(const CBlock& block)
     } else {
         if (pindex->nHeight >= Params().START_POA_BLOCK()) {
             // Bypass bad block
-            if (pindex->nHeight == 719390) {
+            if (pindex->nHeight == 719390 || pindex->nHeight == 719456) {
                 return true;
             }
             CBlock prevPoablock;
